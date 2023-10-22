63996bce039f63a343c222ccbdcde1c514ead7a6
Can Temizyurek <can@temizyurek.com>
2023-10-21 21:14:57 -0400

    setup for npm

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Engine from './engine/Engine'
+import Canvas from './canvas/Canvas'
+
+import Node from './node/Node'
+import RectangleNode from './node/shapes/RectangleNode'
+import CircleNode from './node/shapes/CircleNode'
+
+import Vertex2D from './geometry/Vertex2D'
+
+export { Engine, Canvas, Node, RectangleNode, CircleNode, Vertex2D }
